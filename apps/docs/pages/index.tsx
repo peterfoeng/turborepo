e91bb24bc49eba86df2a02837b09d97373e17243
Peter Foeng <peter@peterfoeng.com>
2022-10-14 15:58:50 +1100

    wip: feature update

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Docs() {
-  return (
-    <div>
-      <h1>Docs (updated 14/10/2022)</h1>
-      <Button />
-    </div>
-  );
+    return (
+        <div>
+            <h1>Docs (updated 14/10/2022)</h1>
+            <p>Implement feature-b (14/10/2022 3:56pm)</p>
+            <Button/>
+        </div>
+    );
 }
