3a51770fdddec8eeb2f04fbb4bbc9cd68bb071fc
Peter Foeng <peter@peterfoeng.com>
2022-10-14 16:00:09 +1100

    feat: add body copy line

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -5,6 +5,7 @@ export default function Docs() {
         <div>
             <h1>Docs (updated 14/10/2022)</h1>
             <p>Implement feature-b (14/10/2022 3:56pm)</p>
+            <p>Implement feature-b extra (14/10/2022 3:58pm)</p>
             <Button/>
         </div>
     );
