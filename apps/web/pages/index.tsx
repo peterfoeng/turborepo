e160b73c1290adc065d1516e524e8d3cc8017532
Peter Foeng <peter@peterfoeng.com>
2022-11-14 14:41:20 +1100

    feat: body content update

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Web() {
   return (
         <div>
             <h1>Web with Turborepo (14 Nov 2022 2:34PM)</h1>
-            <p>love turborepo</p>
+            <p>We all love turborepo</p>
             <Button />
         </div>
   );
