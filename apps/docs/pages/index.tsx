da91fc896cf93d54b3f50a83d8aa1c587d861a78
Peter Foeng <peter@peterfoeng.com>
2022-11-14 14:11:38 +1100

    fix: remove closing tag error

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -6,8 +6,8 @@ export default function Docs() {
             <h1>Docs (updated 14/10/2022)</h1>
             <p>Implement feature-b (14/10/2022 3:56pm)</p>
             <p>Implement feature-b extra (14/10/2022 3:58pm)</p>
-            <div>Implement NEW FEATURE (14/11/2022 1:15pm)</p></div>
-            <div>Implement NEW FEATURE 2nd LINE (14/11/2022 1:20pm)</p></div>
+            <div>Implement NEW FEATURE (14/11/2022 1:15pm)</div>
+            <div>Implement NEW FEATURE 2nd LINE (14/11/2022 1:20pm)</div>
             <Button/>
         </div>
     );
