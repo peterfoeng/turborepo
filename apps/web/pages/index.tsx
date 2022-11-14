63fa87938fe07062321908d74b27989e3ed4d8b9
Peter Foeng <peter@peterfoeng.com>
2022-11-14 14:34:16 +1100

    feat: remove old content

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -6,12 +6,6 @@ export default function Web() {
             <h1>Web with Turborepo (14/10/2022 3.31pm)</h1>
             <p>love turborepo</p>
             <Button />
-      
-            <h2>Web Page update Monday 14 Nov 2022 (1:21PM)</h2>
-            <h3>Web Page update Monday 14 Nov 2022 (V3) (1:35PM)</h3>
-
-            <h4>Web Page update Monday 14 Nov 2022 (V3) (1:35PM)</h4>
-      
         </div>
   );
 }
