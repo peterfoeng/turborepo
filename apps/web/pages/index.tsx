4b1a87929c34a69e63acb8434b3417e4be9e84b8
Peter Foeng <peter@peterfoeng.com>
2022-11-14 14:34:38 +1100

    feat: web heading update

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from "ui";
 export default function Web() {
   return (
         <div>
-            <h1>Web with Turborepo (14/10/2022 3.31pm)</h1>
+            <h1>Web with Turborepo (14 Nov 2022 2:34PM)</h1>
             <p>love turborepo</p>
             <Button />
         </div>
